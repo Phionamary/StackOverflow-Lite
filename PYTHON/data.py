0aff619e292ccd67f6187616c25e76be7c0f14f8
Phionamary <phionamarykigai@gmail.com>
2018-08-15 17:42:41 +0300

    Catch  up with API basics first

---
+++ PYTHON/data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi reiciendis reprehenderit repellat tenetur ipsum beatae corrupti corporis natus a fuga illum harum accusantium laborum aspernatur, sequi, ut tempora nesciunt itaque.',
+            'author': 'Phii',
+            'create_date': '10-09-2017',
+        },
+
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi reiciendis reprehenderit repellat tenetur ipsum beatae corrupti corporis natus a fuga illum harum accusantium laborum aspernatur, sequi, ut tempora nesciunt itaque.',
+            'author': 'Winy',
+            'create_date': '30-09-2017',
+        },
+
+        {
+            'id': '3',
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi reiciendis reprehenderit repellat tenetur ipsum beatae corrupti corporis natus a fuga illum harum accusantium laborum aspernatur, sequi, ut tempora nesciunt itaque.',
+            'author': 'Jackie',
+            'create_date': '16-11-2017',
+        },
+
+    ]
+
+    return articles
