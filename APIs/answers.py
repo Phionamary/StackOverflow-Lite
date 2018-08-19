13c0f37608c5316b8fd17f25910fec1956575c9e
Phionamary <phionamarykigai@gmail.com>
2018-08-19 23:26:25 +0300

    Create endpoint for adding answers to questions

--- APIs/answers.py
+++ APIs/answers.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, json
 
 from questions import questions
 
@@ -10,17 +10,16 @@ answers = [
     {'id': 3, 'answer': [{'ans_id':3,'answer':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi reiciendis reprehenderit repellat tenetur ipsum beatae corrupti corporis natus a fuga illum harum accusantium laborum aspernatur, sequi, ut tempora nesciunt itaque.'}
 ]
 
-@app.route('/api/v1/questions/<int:qn_id>/answer', methods={'POST'})
+@app.route("/api/v1/questions/2/answers", methods=["POST"])
 
-def add_answer(id):
-    particular_question = answers[id - 1]
-    all_answers = particular_question['answer']
-
-    given_answer = request.get_json()
-    if valid_answer(given_answer):
-        new_answer={'ans_id':(len(all_answers)+1),'answer':given_answer["answer"]}
-        all_answers.append(new_answer)
-        return jsonify({'answers': answers})
+def add_answer_to_question():
+    """Adds answer to question."""
+    answers = {
+        "id": request.json["id"],
+        "answer": request.json["answer"]
+    }
+    QUESTION["id" == 2]["answer"].update(answers)
+    return jsonify(answers)
 
 if __name__ == '__main__':
     app.run(debug=True)
